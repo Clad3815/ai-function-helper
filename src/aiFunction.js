9f2168f6bfd0a7a9b59d1591bb5f6148a256c4a3
Clad3815 <jojo38500@hotmail.fr>
2023-05-05 01:29:01 +0200

    Optimise output management + code optimisation

--- src/aiFunction.js
+++ src/aiFunction.js
@@ -42,6 +42,7 @@ function createAiFunctionInstance(apiKey) {
                 top_p = null,
                 max_tokens = null,
                 blockHijack = false,
+                autoRetry = false,
                 promptVars = {},
                 current_date_time = new Date().toISOString(),
         } = options;
@@ -60,13 +61,7 @@ function createAiFunctionInstance(apiKey) {
             };
         }
         argsString = formatObjectArgs(args);
-
-        if (getType(argsString) === 'str') {
-            argsString = argsString.replace(/true/g, 'True').replace(/false/g, 'False');
-            // Replace all line break by \n
-            argsString = argsString.replace(/(\r\n|\n|\r)/gm, "\\n");
-        }
-
+        argsString = argsString.replace(/true/g, 'True').replace(/false/g, 'False').replace(/(\r\n|\n|\r)/gm, "\\n");
 
 
         if (!funcArgs) {
@@ -76,11 +71,11 @@ function createAiFunctionInstance(apiKey) {
         let isJson = '';
         if (autoConvertReturn === true) {
             isJson = ' converted into a valid JSON string adhering to UTF-8 encoding using the python json.dumps() function';
+            if (funcReturn === 'str') {
+                funcReturnString = 'dict[return:str]';
+            }
         }
 
-        if (funcReturnString === 'str') {
-            isJson = ' without surrounding quotes';
-        }
 
         for (const [key, value] of Object.entries(promptVars)) {
             description = description.replace('${' + key + '}', value);
@@ -122,7 +117,8 @@ function createAiFunctionInstance(apiKey) {
             console.log(chalk.magenta('With arguments: ') + chalk.green(messages[1]['content'].trim()));
             console.log(chalk.yellow('####################'));
         }
-        const gptResponse = await retry(() => openai.createChatCompletion({
+
+        const apiCall = () => openai.createChatCompletion({
             model: model,
             messages: messages,
             temperature: temperature,
@@ -130,44 +126,19 @@ function createAiFunctionInstance(apiKey) {
             presence_penalty: presence_penalty,
             max_tokens: max_tokens,
             top_p: top_p,
-        }));
+        });
+
+        let gptResponse = await (autoRetry ? retry(apiCall) : apiCall());
+
 
         let answer = gptResponse.data.choices[0]['message']['content'];
 
         if (autoConvertReturn === true) {
-            if (answer.startsWith("```python") && answer.endsWith("```")) {
-                answer = answer.substring(9, answer.length - 3);
-            } else if (answer.startsWith("```json") && answer.endsWith("```")) {
-                answer = answer.substring(7, answer.length - 3);
-            } else if (answer.startsWith("`'") && answer.endsWith("'`")) {
-                answer = answer.substring(2, answer.length - 2);
-            } else if (answer.startsWith("`\"") && answer.endsWith("\"`")) {
-                answer = answer.substring(2, answer.length - 2);
-            } else if (answer.startsWith("```") && answer.endsWith("```")) {
-                answer = answer.substring(3, answer.length - 3);
-            } else if (answer.startsWith("``") && answer.endsWith("``")) {
-                answer = answer.substring(2, answer.length - 2);
-            } else if (answer.startsWith("`") && answer.endsWith("`")) {
-                answer = answer.substring(1, answer.length - 1);
-            } else if (answer.startsWith('""') && answer.endsWith('""')) {
-                answer = answer.substring(2, answer.length - 2);
-            } else if (answer.startsWith('"') && answer.endsWith('"')) {
-                answer = answer.substring(1, answer.length - 1);
-            } else if (answer.startsWith("'") && answer.endsWith("'")) {
-                answer = answer.substring(1, answer.length - 1);
-            }
+            answer = answer.replace(/^(```(?:python|json)?|`['"]?|['"]?)|(```|['"`]?)$/g, '');
+
 
             if (answer.startsWith("return json.dumps(") && answer.endsWith(")")) {
                 answer = answer.substring(18, answer.length - 1);
-            }
-
-            if (funcReturnString === 'str') {
-                if (showDebug) {
-                    console.log(chalk.yellow('####################'));
-                    console.log(chalk.blue('Returning brut answer: ' + answer));
-                    console.log(chalk.yellow('####################'));
-                }
-                return answer;
             }
             if (isValidJSON(answer)) {
                 if (showDebug) {
@@ -175,7 +146,12 @@ function createAiFunctionInstance(apiKey) {
                     console.log(chalk.green('Valid JSON, returning it: ' + answer));
                     console.log(chalk.green('####################'));
                 }
-                return parseJson(answer);
+                if (funcReturn === 'str') {
+                    let parsedAnswer = parseJson(answer);
+                    return parsedAnswer.return;
+                } else {
+                    return parseJson(answer);
+                }
             } else {
                 if (showDebug) {
                     console.log(chalk.yellow('####################'));
@@ -183,7 +159,12 @@ function createAiFunctionInstance(apiKey) {
                 }
                 let fixedAnswer = await fixBadJsonFormat(answer.trim(), showDebug);
                 if (fixedAnswer !== "") {
-                    return parseJson(fixedAnswer);
+                    if (funcReturn === 'str') {
+                        let parsedAnswer = parseJson(fixedAnswer);
+                        return parsedAnswer.return;
+                    } else {
+                        return parseJson(fixedAnswer);
+                    }
                 } else {
                     if (showDebug) {
                         console.log(chalk.red('Could not fix JSON'));
@@ -203,59 +184,25 @@ function createAiFunctionInstance(apiKey) {
 
     return aiFunction;
 }
-async function fixJsonString(pythonString) {
-    // Trim the string
-    pythonString = pythonString.trim();
 
-    // Replace single quotes that are not preceded or followed by word characters with double quotes
-    let jsonString = pythonString.replace(/(^|[^\\w])'($|[^\\w])/g, '$1"$2');
-
-    // Check if the string start with "{" and end with "}," if yes delete the "," at the end
-    if (jsonString.startsWith("{") && jsonString.endsWith("},")) {
-        jsonString = jsonString.substring(0, jsonString.length - 1);
-    }
-
-    // Replace escaped single quotes with single quotes
-    jsonString = jsonString.replace(/\\"/g, "'");
-
-    // Replace all ” with "
-    jsonString = jsonString.replace(/”/g, '"');
-
-    // Replace all “ with "
-    jsonString = jsonString.replace(/“/g, '"');
-
-    // Replace all ‘ with '
-    jsonString = jsonString.replace(/‘/g, "'");
-
-    // Replace all ’ with '
-    jsonString = jsonString.replace(/’/g, "'");
-
-    // Fix unescaped double quotes within strings
-    jsonString = jsonString.replace(/(\w)"(\w)/g, '$1\\"$2');
-
-    // Replace all \' with '
-    jsonString = jsonString.replace(/\\'/g, "'");
-
-    // Replace all None with null
-    jsonString = jsonString.replace(/None/g, 'null');
-
-    // Replace all "True" with true and "False" with false
-    jsonString = jsonString.replace(/True/g, 'true');
-    jsonString = jsonString.replace(/False/g, 'false');
-
-    // Delete all ` from the start and the end of the string (1 or more)
-    jsonString = jsonString.replace(/^`+|`+$/g, '');
-
-    // Delete all ' from the start and the end of the string (1 or more)
-    jsonString = jsonString.replace(/^'+|'+$/g, '');
-
-    return jsonString;
+function fixJsonString(pythonString) {
+    return pythonString.trim()
+        .replace(/(^|[^\\w])'($|[^\\w])/g, '$1"$2')
+        .replace(/\\"/g, "'")
+        .replace(/[”“]/g, '"').replace(/[‘’]/g, "'")
+        .replace(/(\w)"(\w)/g, '$1\\"$2')
+        .replace(/\\'/g, "'")
+        .replace(/None/g, 'null')
+        .replace(/True/g, 'true').replace(/False/g, 'false')
+        .replace(/^`+|`+$/g, '')
+        .replace(/^'+|'+$/g, '')
+        .replace(/(^\{.*),\}$/g, '$1}');
 }
 
 
 async function fixBadJsonFormat(jsonString, showDebug = false) {
 
-    const tryFixJsonString = await fixJsonString(jsonString);
+    const tryFixJsonString = fixJsonString(jsonString);
     if (tryFixJsonString !== jsonString) {
         if (isValidJSON(tryFixJsonString)) {
             if (showDebug) {
@@ -292,50 +239,33 @@ async function fixBadJsonFormat(jsonString, showDebug = false) {
 }
 
 function convertArgs(args) {
-    let funcArgs = '';
-
     const type = getType(args);
 
-    if (type === 'list') {
-        for (let i = 0; i < args.length; i++) {
-            const argType = getType(args[i]);
-            funcArgs += `${String.fromCharCode(97 + i)}: ${argType}`;
-            if (i < args.length - 1) {
-                funcArgs += ', ';
-            }
-        }
-    } else if (type === 'dict') {
-        const keys = Object.keys(args);
-        for (let i = 0; i < keys.length; i++) {
-            const key = keys[i];
-            const argType = getType(args[key]);
-            funcArgs += `${key}: ${argType}`;
-            if (i < keys.length - 1) {
-                funcArgs += ', ';
-            }
-        }
-        // Else if type is float or the string contain a valid float number
-    } else if (type === 'float' || (type === 'str' && !isNaN(parseFloat(args)))) {
-        funcArgs = 'f: float';
-    } else if (type === 'str') {
-        funcArgs = 's: str';
-    } else if (type === 'int') {
-        funcArgs = 'i: int';
-    } else if (type === 'bool') {
-        funcArgs = 'b: bool';
-
-    } else {
-        funcArgs = 'a: Anything';
+    switch (type) {
+        case 'list':
+            return args.map((arg, i) => `${String.fromCharCode(97 + i)}: ${getType(arg)}`).join(', ');
+        case 'dict':
+            return Object.keys(args).map(key => `${key}: ${getType(args[key])}`).join(', ');
+        case 'float':
+        case 'str':
+            return !isNaN(parseFloat(args)) ? 'f: float' : 's: str';
+        case 'int':
+            return 'i: int';
+        case 'bool':
+            return 'b: bool';
+        default:
+            return 'a: Anything';
     }
-
-    return funcArgs;
 }
+
 
 function formatArg(arg) {
     const type = getType(arg);
 
-    if (type === 'str' || type === 'int' || type === 'bool' || type === 'float') {
+    if (type === 'str') {
         return `"${arg}"`;
+    } else if (type === 'int' || type === 'bool' || type === 'float') {
+        return arg;
     } else if (type === 'list' || type === 'dict') {
         return JSON.stringify(arg);
     } else if (type === 'undefined' || type === 'null' || type === 'unknown') {
@@ -355,22 +285,22 @@ function formatObjectArgs(obj) {
 function getType(value) {
     const type = Object.prototype.toString.call(value);
 
-    if (type === '[object Array]') {
-        return 'list';
-    } else if (type === '[object Object]') {
-        return 'dict';
-    } else if (type === '[object String]') {
-        return 'str';
-    } else if (type === '[object Number]' && value % 1 !== 0) {
-        return 'float';
-    } else if (type === '[object Number]') {
-        return 'int';
-    } else if (type === '[object Boolean]') {
-        return 'bool';
-    } else {
-        return 'unknown';
+    switch (type) {
+        case '[object Array]':
+            return 'list';
+        case '[object Object]':
+            return 'dict';
+        case '[object String]':
+            return 'str';
+        case '[object Number]':
+            return value % 1 !== 0 ? 'float' : 'int';
+        case '[object Boolean]':
+            return 'bool';
+        default:
+            return 'unknown';
     }
 }
+
 
 async function retry(fn, retries = 3, delay = 1000) {
     try {
